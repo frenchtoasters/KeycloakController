424f9a10261ec49ad47423500159ba389e1f5edc
toast <toast@big-toaster>
2024-02-10 23:11:57 -0500

    Use copier library for relection instead of manually copying

--- cloud/services/keycloak/utils/transform.go
+++ cloud/services/keycloak/utils/transform.go
@@ -3,65 +3,23 @@ package utils
 import (
 	appdatv1alpha1 "appdat.jsc.nasa.gov/platform/controllers/mri-keycloak/api/v1alpha1"
 	"github.com/Nerzal/gocloak/v13"
+	"github.com/jinzhu/copier"
 )
 
 func UserTransform(user *appdatv1alpha1.User) gocloak.User {
 	gocloakUser := &gocloak.User{}
-	gocloakUser.Access = user.Access
-	gocloakUser.Totp = user.Totp
-	gocloakUser.Email = user.Email
-	gocloakUser.Groups = user.Groups
-	gocloakUser.Enabled = user.Enabled
-	gocloakUser.LastName = user.LastName
-	gocloakUser.Username = user.Username
-	gocloakUser.FirstName = user.FirstName
-	gocloakUser.Attributes = user.Attributes
-	gocloakUser.RealmRoles = user.RealmRoles
-	gocloakUser.ClientRoles = user.ClientRoles
-	gocloakUser.EmailVerified = user.EmailVerified
-	gocloakUser.FederationLink = user.FederationLink
-	gocloakUser.RequiredActions = user.RequiredActions
-	gocloakUser.CreatedTimestamp = user.CreatedTimestamp
-	gocloakUser.ServiceAccountClientID = user.ServiceAccountClientID
-
-	// if *user.Credentials != nil {
-	// 	for i := 0; i < len(*user.Credentials); i++ {
-	// 		(*gocloakUser.Credentials)[i].CreatedDate = (*user.Credentials)[i].CreatedDate
-	// 		(*gocloakUser.Credentials)[i].Temporary = (*user.Credentials)[i].Temporary
-	// 		(*gocloakUser.Credentials)[i].Type = (*user.Credentials)[i].Type
-	// 		(*gocloakUser.Credentials)[i].Value = (*user.Credentials)[i].Value
-	// 		(*gocloakUser.Credentials)[i].CredentialData = (*user.Credentials)[i].CredentialData
-	// 		(*gocloakUser.Credentials)[i].ID = (*user.Credentials)[i].ID
-	// 		(*gocloakUser.Credentials)[i].Priority = (*user.Credentials)[i].Priority
-	// 		(*gocloakUser.Credentials)[i].SecretData = (*user.Credentials)[i].SecretData
-	// 		(*gocloakUser.Credentials)[i].UserLabel = (*user.Credentials)[i].UserLabel
-	// 	}
-	// }
+	copier.Copy(&gocloakUser, user)
 	return *gocloakUser
 }
 
 func GroupTransform(group *appdatv1alpha1.Group) gocloak.Group {
 	gocloakGroup := &gocloak.Group{}
-	gocloakGroup.Name = group.Name
-	gocloakGroup.Access = group.Access
-	gocloakGroup.Path = group.Path
-	gocloakGroup.Attributes = group.Attributes
-	gocloakGroup.RealmRoles = group.RealmRoles
-	gocloakGroup.ClientRoles = group.ClientRoles
-
+	copier.Copy(&gocloakGroup, group)
 	return *gocloakGroup
 }
 
 func RoleTransform(role *gocloak.Role) *appdatv1alpha1.Role {
 	appdatrole := &appdatv1alpha1.Role{}
-	appdatrole.ID = role.ID
-	appdatrole.ContainerID = role.ContainerID
-	appdatrole.Name = role.Name
-	appdatrole.Composite = role.Composite
-	appdatrole.Composites = (*appdatv1alpha1.CompositesRepresentation)(role.Composites)
-	appdatrole.Description = role.Description
-	appdatrole.Attributes = role.Attributes
-	appdatrole.ClientRole = role.ClientRole
-	appdatrole.ScopeParamRequired = role.ScopeParamRequired
+	copier.Copy(&appdatrole, role)
 	return appdatrole
 }
--- go.mod
+++ go.mod
@@ -4,9 +4,11 @@ go 1.20
 
 require (
 	github.com/Nerzal/gocloak/v13 v13.8.0
+	github.com/jinzhu/copier v0.4.0
 	github.com/onsi/ginkgo/v2 v2.13.1
 	github.com/onsi/gomega v1.30.0
 	github.com/pkg/errors v0.9.1
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9
 	golang.org/x/oauth2 v0.14.0
 	k8s.io/api v0.28.4
 	k8s.io/apimachinery v0.28.4
@@ -57,7 +59,6 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.25.0 // indirect
-	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
 	golang.org/x/net v0.18.0 // indirect
 	golang.org/x/sys v0.14.0 // indirect
 	golang.org/x/term v0.14.0 // indirect
